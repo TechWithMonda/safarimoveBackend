cf04d974381eeed3f5b566ef7f9925f70937aabe
MondaSamuel <samwelmonda48@gmail.com>
2025-07-26 14:05:48 +0300

    postgrss

--- nairobi_connect/settings.py
+++ nairobi_connect/settings.py
@@ -134,12 +134,6 @@ WSGI_APPLICATION = 'nairobi_connect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
